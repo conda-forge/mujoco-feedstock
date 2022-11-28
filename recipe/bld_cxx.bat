58723e6d7dcaad82584f801478eb5ebdbf717e4e
Silvio Traversaro <silvio@traversaro.it>
2022-11-28 04:57:34 +0100

    Update bld_cxx.bat

--- recipe/bld_cxx.bat
+++ recipe/bld_cxx.bat
@@ -28,6 +28,11 @@ if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
+:: Cleanup plugin install
+del %LIBRARY_PREFIX%\bin\mujuco_plugin\*.lib
+
 :: Test.
 ctest --output-on-failure -C Release 
 if errorlevel 1 exit 1
+
+
